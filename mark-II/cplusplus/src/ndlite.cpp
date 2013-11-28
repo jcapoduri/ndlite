a28ddf13ca828889dc6d2bb8035643e9bc354f56
jcapoduri <jcapoduril@gmail.com>
2013-11-28 20:56:27 -0300

    remove old backup

--- mark-II/cplusplus/src/ndlite.cpp
+++
@@ -1,1 +0,0 @@
-#include "ndlite.h"
--- mark-II/cplusplus/src/storage.cpp
+++
@@ -1,7 +0,0 @@
-#include "storage.h"
-
-nd::storage::storage(QJsonValue &initdata, app *application, QObject *parent) : QObject(parent)
-{
-    raw_data = initdata;
-    _app = application;
-}
